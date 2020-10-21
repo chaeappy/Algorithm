660dd343190285691d76b8bd648b1f29486ecd42
chaeappy <cheappy@gmail.com>
2020-10-22 00:30:05 +0900

    ceil함수 만듦

--- src/A9/Math1/A2869.java
+++ src/A9/Math1/A2869.java
@@ -7,34 +7,29 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class A2869 {
-	static int tired (double[] doubleArr) {
-		double day = 0;
-		if ((doubleArr[2] - doubleArr[0]) % (doubleArr[0] - doubleArr[1]) != 0) {
-			day = ((doubleArr[2] - doubleArr[0]) / (doubleArr[0] - doubleArr[1])) + 2;
-			
-		} else {
-			day = ((doubleArr[2] - doubleArr[0]) / (doubleArr[0] - doubleArr[1])) + 1;
-		}
-		return (int) day;
-	}
-		
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String msg = br.readLine();
-		String[] strArr = msg.split(" ");
-		double[] doubleArr = new double[strArr.length];
-		for (int i = 0; i < strArr.length; i++) {
-			doubleArr[i] = Integer.parseInt(strArr[i]);
-		}
-		
-		
-		int dday = tired(doubleArr);
-		bw.write(dday + "");
-		bw.flush();
-		bw.close();
-		
+	static double cceil(double day) {
+		double dday = (int) day + 1;
+		return dday;
 		
 	}
+      
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      String msg = br.readLine();
+      String[] strArr = msg.split(" ");
+      double[] doubleArr = new double[strArr.length];
+      for (int i = 0; i < strArr.length; i++) {
+         doubleArr[i] = Integer.parseInt(strArr[i]);
+      }
+      
+      double day = ((doubleArr[2] - doubleArr[0]) / (doubleArr[0] - doubleArr[1])) + 1;
+      int dday = (int) cceil(day);
+      bw.write(dday + "");
+      bw.flush();
+      bw.close();
+      
+      
+   }
 
 }
