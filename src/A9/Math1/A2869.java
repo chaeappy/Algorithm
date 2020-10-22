fe4a7cf4436421be3b22f14a0e15cc203b1672c9
chaeappy <cheappy@gmail.com>
2020-10-22 10:37:32 +0900

    A2869 ceil 함수 변경 >> 제출시 정답

--- src/A9/Math1/A2869.java
+++ src/A9/Math1/A2869.java
@@ -7,29 +7,34 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class A2869 {
-	static double cceil(double day) {
-		double dday = (int) day + 1;
-		return dday;
+	static double calculateCeil(double day) {
+		if (day % (int) day == 0) {
+			return day;
+		} else {
+			day = (int) day + 1;
+			return day;
+		}
+	}
+	
+      
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String msg = br.readLine();
+		String[] strArr = msg.split(" ");
+		double[] doubleArr = new double[strArr.length];
+		for (int i = 0; i < strArr.length; i++) {
+			doubleArr[i] = Integer.parseInt(strArr[i]);
+		}
+		
+		double day = ((doubleArr[2] - doubleArr[0]) / (doubleArr[0] - doubleArr[1])) + 1;
+		int dday = (int) (calculateCeil(day));
+		bw.write(dday + "");
+		bw.flush();
+		bw.close();
+		
 		
 	}
-      
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-      String msg = br.readLine();
-      String[] strArr = msg.split(" ");
-      double[] doubleArr = new double[strArr.length];
-      for (int i = 0; i < strArr.length; i++) {
-         doubleArr[i] = Integer.parseInt(strArr[i]);
-      }
-      
-      double day = ((doubleArr[2] - doubleArr[0]) / (doubleArr[0] - doubleArr[1])) + 1;
-      int dday = (int) cceil(day);
-      bw.write(dday + "");
-      bw.flush();
-      bw.close();
-      
-      
-   }
 
 }
+
